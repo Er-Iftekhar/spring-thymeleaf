1ae927ec17a4a7a3602ced6fcca5c42948e68112
SyedIftekharuddin <syedy636@gmail.com>
2020-08-21 21:22:07 +0200

    Initial project add

---
+++ src/main/java/com/app/utb/springthymeleafcourse/SpringThymeleafCourseApplication.java
@@ -0,0 +1,13 @@
+package com.app.utb.springthymeleafcourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeleafCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringThymeleafCourseApplication.class, args);
+    }
+
+}
